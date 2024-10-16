8c60e731d8a2b488c6d1967bb615b2de734a52bd
Juan Keny <juankeny@outlook.com>
2024-10-16 15:14:58 +0100

    Made a basic window

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import pygame
+pygame.init()
 
-print("Hello world")
 
+screen = pygame.display.set_mode([400,200])
+
+running = True
+while running:
+
+   
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    
+    
+    pygame.display.update()
+
+
+pygame.quit()
